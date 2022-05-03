923d7e4224d96d6e40af9c35b59239b6057179b2
Guilherme Campos <guilhermee1219@gmail.com>
2022-05-03 19:31:32 -0300

    Aula 2 criando card

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { ChatTeardropDots, Heart } from 'phosphor-react';
 import { Popover } from '@headlessui/react'
+import { WidgetForm } from './WidgetForm';
 
 // group: 
 export function Widget() {
@@ -12,11 +13,9 @@ export function Widget() {
 
 
   return (
-    <Popover className='absolute -bottom-5 right-5'>
+    <Popover className='absolute -bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end'>
       <Popover.Panel className={'flex items-center justify-center'}>
-        <Heart />
-        <p>Welcome</p>
-        <Heart />
+        <WidgetForm />
       </Popover.Panel>
 
       <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>

0e41f2841f0b47c304901595c2f8c5d3d4a0f46b
Guilherme Campos <guilhermee1219@gmail.com>
2022-05-02 22:55:55 -0300

    Aula 01

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { App } from './App'
+
 import './global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -8,3 +9,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>
 )
+
